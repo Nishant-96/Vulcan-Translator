3e20ff031afd350c6906f1404c74ff63c71cafc8
Nishant-96 <nishant11pratihast@gmail.com>
2021-07-25 01:04:24 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,5 @@
+var translateButton = doucment.querySelector("#translate-button");
+var inputText = document.querySelector("#text-input");
+var outputBox = document.querySelector("#output");
+
+var serverURL = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json"
