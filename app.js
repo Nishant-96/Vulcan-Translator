e5843e55fc28547267f6b9bd9bacdf896e68838c
Nishant-96 <nishant11pratihast@gmail.com>
2021-07-25 01:41:31 +0530

    update: apps functionality done.

--- app.js
+++ app.js
@@ -1,5 +1,34 @@
-var translateButton = doucment.querySelector("#translate-button");
-var inputText = document.querySelector("#text-input");
-var outputBox = document.querySelector("#output");
+var translateButton = document.querySelector("#translate-button");
+var textInput = document.querySelector("#text-input");
+var outputBox = document.querySelector("#outputBox");
 
-var serverURL = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json"
+var serverURL = "https://api.funtranslations.com/translate/vulcan.json"
+
+
+
+function constructedTranslationURL(text){
+    return serverURL+"?"+"text="+text
+}
+
+
+translateButton.addEventListener("click", clickHandler)
+
+function clickHandler(){
+    var textToBeTranslated=textInput.value;             //input
+
+    //server processing
+
+    fetch(constructedTranslationURL(textToBeTranslated))
+        .then(response=>response.json())
+        .then(json=>{
+            var translatedText = json.contents.translated;
+            outputBox.innerText= translatedText;            //output
+        })
+        .catch(errorHandler)
+}
+
+//error handling
+function errorHandler(error){
+    console.log("Error Occured",error);
+    alert("Error: Server Issue Try Again After Sometime")
+}
